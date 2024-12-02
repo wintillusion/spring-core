28a3ce534752d61b243eaef1d6ffcc625c738875
chs <chs@sotongfive.kr>
2024-12-02 15:45:16 +0900

    git

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,4 +1,20 @@
 package hello.core.discount;
 
-public class FixDiscountPolicy {
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){     /*enum은 ==로 구분*/
+            return price - discountFixAmount;
+        }else{
+            return 0;
+        }
+
+
+    }
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,4 +1,27 @@
 package hello.core.order;
 
-public class OrderServiceImpl {
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+
+        int discountPrice = discountPolicy.discount(member,itemPrice);  /*member 통으로 넘겨서 추후 확장성 체크*/
+        /*설계가 매우 잘된 형태*/
+        /*오더 서비스 입장에서는 할인 에 대한 것은 discountPolicy가 알아서 처리- 단일체계*/
+        /*할인 문제는 할인쪽에서만 수정을 해서 유지보수 용이하게 수정을 하면 됨*/
+        /*이게 구분이 안되어 있으면 오더서비스도 수정해야 됐을 것*/
+
+        return new Order(memberId, itemName,itemPrice, discountPrice);
+
+    }
 }
