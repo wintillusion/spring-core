1ea5b1924febd775093e6f9af7b7c5308c255c17
chs <chs@sotongfive.kr>
2024-12-03 16:52:44 +0900

    spring bean singleton 검증

--- src/test/java/hello/core/singleton/SingleTonTest.java
+++ src/test/java/hello/core/singleton/SingleTonTest.java
@@ -5,6 +5,7 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -38,6 +39,19 @@ public class SingleTonTest {
         /*같은 객체*/
         assertThat(singletonServiceTest).isSameAs(singletonServiceTest2);
         /*same : ==*/
+    }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+//        AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
