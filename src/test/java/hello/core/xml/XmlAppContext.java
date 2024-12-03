6b8166a1dbdecd48b16c1bd5585739a31cac692b
chs <chs@sotongfive.kr>
2024-12-03 14:50:53 +0900

    spring config xml test

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,4 +1,20 @@
 package hello.core.xml;
 
+import hello.core.member.MemberService;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+        /*java file이 아니기 때문에 xml은 resources에 등록*/
+    }
 }
