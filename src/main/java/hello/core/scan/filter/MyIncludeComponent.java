9c37a5fbf9d7884fc2535882b8cc826b478873f6
chs <chs@sotongfive.kr>
2024-12-04 15:20:55 +0900

    componentScan filter 적용

---
+++ src/main/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {  /*컴포넌트 스캔 추가 대상*/
+}
