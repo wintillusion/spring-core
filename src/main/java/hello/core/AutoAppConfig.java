3d9bac9187740b7191cfd42b96f85221f0bba9fa
chs <chs@sotongfive.kr>
2024-12-05 16:28:17 +0900

    빈 모두 조회-할인 선택 로직 적용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration  /*이 애노테이션 안에도 Component가 있음*/
 @ComponentScan(
-        basePackages = "hello.core.member", /*탐색 위치를 지정할 수 있고 {"hello.core", "hello.service"} 이렇게 여러 위치를 지정할 수 있음. 만약 지정하지 않으면 본인 위치에서부터 스캔 시작*/
+        basePackages = "hello.core", /*탐색 위치를 지정할 수 있고 {"hello.core", "hello.service"} 이렇게 여러 위치를 지정할 수 있음. 만약 지정하지 않으면 본인 위치에서부터 스캔 시작*/
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
         /*필터를 통해 Configuration 애노테이션이 붙은걸 뺀다(AutoAppConfig과 AppConfig 충돌 방지)*/
 )  /*@Compoent 클래스를 찾아서 자동으로 spring bean으로 등록하는 애노테이션*/
---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,61 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+/*예를 들어 모든 빈을 조회하는 경우*/
+/*할인 서비스를 제공하는데, 클라이언트가 할인의 종류를 고를 수 있을 때(rate, fix)
+* Spring을 사용하면 소위 말하는 '전략 패턴'을 간단히 구현 가능하다.
+* */
+
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class); /*만든 클래스 등록*/
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member,10000,"fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+
+    /*@RequiredArgsConstructor 해도 됨*/
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member,price);
+        }
+    }
+}
