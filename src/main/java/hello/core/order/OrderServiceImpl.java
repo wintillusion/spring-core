a75f3081a2238c6f8a684724526cf3ef7f081396
chs <chs@sotongfive.kr>
2024-12-05 15:29:37 +0900

    lombok 적용(@RequiredArgsConstructor)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,29 +5,19 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor /*final로 선언된 변수를 가지고 직접 생성자를 만들어줌.*/
 public class OrderServiceImpl implements OrderService {
 
-//    private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
-    /*final 선언인 경우 기본으로 무조건  할당하든 아래처럼 생성자를 통해 할당하든 되어야함.*/
-    /*인터페이스만 참조하고 있음. DIP 준수 중*/
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    /*AppConfig에서 받은것을 final 변수에 할당*/
-    @Autowired  /*spring 컨테이너에서 memberRepository alc discountPoly도 직접 주입을 해준다... autowired의 역할*/
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
 
-        /*FixDiscount인지, RateDiscount인지는 모름!!!*/
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
